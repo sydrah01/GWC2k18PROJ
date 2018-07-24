9ba8d39f912d91d4c8e296cb54db572b74e271b3
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2018-07-23 22:17:04 -0700

    Can now move trash with finger
    +got rid of spiny effect
    +can only move one piece of trash at a time
    +made a add piece helper function

--- TrashProject/GameScene.swift
+++ TrashProject/GameScene.swift
@@ -12,20 +12,19 @@ import GameplayKit
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
-    private let garbageImageNames = [
+    private var caughtTrash : SKNode?
+    
+    private let trashImageNames = [
         "diapers",
         "straw"
     ]
     
     override func sceneDidLoad() {
         var i = 0
-        for garbageImageName in garbageImageNames {
-            let garbageNode = SKSpriteNode(imageNamed: garbageImageName)
-            garbageNode.position = CGPoint(x:75 * i, y: 10)
+        for trashImageName in trashImageNames {
+            addPiece(imageName:trashImageName, nodeName: "trash", startingPosition: CGPoint(x:75 * i, y: 10))
             i += 1
-            addChild(garbageNode)
         }
     }
     
@@ -37,64 +36,52 @@ class GameScene: SKScene {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+   }
+    
+    func addPiece(imageName: String, nodeName: String, startingPosition: CGPoint) {
+        let node = SKSpriteNode(imageNamed: imageName)
+        node.name = nodeName
+        node.position = startingPosition
+        addChild(node)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func touchBegin(atPoint pos : CGPoint) {
+        caughtTrash = atPoint(pos)
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+        if let haveCaughtTrash = caughtTrash {
+            haveCaughtTrash.position = pos
         }
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func touchEnd(atPoint pos : CGPoint) {
+        // TODO: Drop into recycle/trash/compost
+        caughtTrash = nil
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            touchBegin(atPoint:touch.location(in: self))
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        if let touch = touches.first {
+            touchMoved(toPoint:touch.location(in: self))
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if let touch = touches.first {
+            touchEnd(atPoint:touch.location(in: self))
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if let touch = touches.first {
+            touchEnd(atPoint:touch.location(in: self))
+        }
     }
     
     
